135a17cb7e5daad550f7a9971bbe86ec37140115
Rustam Ibragimov <iamdaiver@icloud.com>
2018-08-10 23:56:22 +0300

    [specs] simplecov integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
-require "pry"
+require "simplecov"
+require "coveralls"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+])
+
+SimpleCov.start { add_filter "spec" }
+
+require "bundler/setup"
 require "self_data"
+require "pry"
+
+RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+end
