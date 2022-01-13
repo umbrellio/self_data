0e537c582466bfe93e4bbdddc710f01642f990a2
Yuri Smirnov <tycooon@yandex.ru>
2022-01-13 19:46:36 +0300

    fix yaml loading + use GH actions

--- lib/self_data/config.rb
+++ lib/self_data/config.rb
@@ -8,5 +8,5 @@ SelfData.default_formats = [:erb, :yaml]
 SelfData.default_options = {}
 
 SelfData.add_converter :erb, -> (data, options) { ERB.new(data).result(options[:context]) }
-SelfData.add_converter :yaml, -> (data, _options) { YAML.load(data) }
+SelfData.add_converter :yaml, -> (data, _options) { YAML.unsafe_load(data) }
 SelfData.add_converter :json, -> (data, _options) { JSON.parse(data) }
--- self_data.gemspec
+++ self_data.gemspec
@@ -7,6 +7,8 @@ require "self_data/version"
 files_regexp = %r{^(test|spec|features)/}
 
 Gem::Specification.new do |spec|
+  spec.required_ruby_version = ">= 2.6.0"
+
   spec.name          = "self_data"
   spec.version       = SelfData::VERSION
   spec.authors       = ["Umbrellio"]
